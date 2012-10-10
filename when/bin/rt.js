76a4fe1754125689af4b26ce6b10deece29c3687
Adlan Elm <adlan@timpex.no>
2012-10-10 19:32:49 +0200

    runner fix

--- when/bin/rt.js
+++ when/bin/rt.js
@@ -3,8 +3,9 @@
 var cwd = process.cwd();
 var glob = require('glob');
 
-glob(cwd + '/**[!node_modules]/when*.js', function(err, files) {
+glob(cwd + '/**/when*.js', function(err, files) {
     for(var i in files.reverse()) {
+      if( files[i].indexOf('node_modules') >= 0 ) continue;
       require(files[i]);
     }
     process.summary();
