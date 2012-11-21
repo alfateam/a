9a55729335c73af7ae363e5939a2c4c4dc9b2d13
Adlan Elm <adlan@timpex.no>
2012-11-21 21:13:26 +0100

    avoid caching of when module

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+(function avoid_caching_when_module() {
+	delete require.cache[module.id];
+	delete require.cache[require.resolve('./when/when')];
+})();
 
-module.exports =  {
+module.exports = {
 	when: require('./when/when'),
 	mock: require('./mock'),
 	expectRequire: require('./expectRequire'),
 	requireMock: require('./requireMock')
-}
+};
+
