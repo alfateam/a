321b25649fe55f8ec97be7cccfa3c2746d469262
Lars-Erik Roald <lars.roald@systor.st>
2023-01-02 20:06:46 +0100

    forgot to remove a_test

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 (function avoid_caching() {
 	delete require.cache[module.id];
 })();
-var calling_module = module.parent;
-var _when = require('a_test').load(calling_module);
 
 var a_mock = require('a_mock');
 
 
 module.exports = {
-	when: _when,
 	mock: a_mock.mock,
 	expectRequire: a_mock.expectRequire,
 	requireMock: a_mock.requireMock,
